38e9b5d2585553511bb39455c9a0c20c1edb20b4
Mario Lazzari <mario.lazzari@gmail.com>
2021-08-29 19:08:50 +0200

    Done lesson 2

--- src/null-object.js
+++ src/null-object.js
@@ -1,1 +1,44 @@
-console.log("object");
+class User {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  hasAccess() {
+    return this.name === "Bob";
+  }
+}
+
+// null object
+class NullUser {
+  constructor() {
+    this.id = -1;
+    this.name = "Guest";
+  }
+
+  hasAccess() {
+    return false;
+  }
+}
+
+const users = [new User(1, "Bob"), new User(2, "John")];
+
+function getUser(id) {
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return new NullUser();
+  }
+  return user;
+}
+
+function printUser(id) {
+  const user = getUser(id);
+
+  console.log("Hello " + user.name);
+
+  if (user.hasAccess()) {
+    console.log("You have access");
+  } else {
+    console.log("You are not allowed here");
+  }
+}
